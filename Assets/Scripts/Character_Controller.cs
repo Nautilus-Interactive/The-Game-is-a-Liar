0d7c7f5fb52e725072f07e32b321195e0fee2f3c
NautilusInteractive <admin@NautilusInteractive.com>
2019-08-24 17:54:40 -0300

    Added head and mouse control

--- Assets/Scripts/Character_Controller.cs
+++ Assets/Scripts/Character_Controller.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Character_Controller : MonoBehaviour
 {
     CharacterController character_controller;
+    public GameObject head;
     public bool can_jump = true;
+    public bool mouse_turn = true;
     public float air_speed = 4.0f;
     public float ground_speed = 6.0f;
     public float jump_speed = 8.0f;
-
+    public float mouse_sensitivity = 100.0f;
     private Vector3 move_direction = Vector3.zero;
 
     // Start is called before the first frame update
@@ -22,7 +24,12 @@ public class Character_Controller : MonoBehaviour
     void Update()
     {
         float temp = move_direction.y;
-        move_direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        if (mouse_turn) {
+            move_direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        } else {
+            move_direction = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        }
 
         if (character_controller.isGrounded) {
             
@@ -39,6 +46,16 @@ public class Character_Controller : MonoBehaviour
         }
 
         move_direction.y += Physics.gravity.y * Time.deltaTime;
+
+        if (mouse_turn) {
+            Vector3 body_rotate = new Vector3(0.0f, Input.GetAxis("Mouse X"), 0.0f) * mouse_sensitivity;
+            Vector3 head_rotate = new Vector3(-Input.GetAxis("Mouse Y"), 0.0f, 0.0f) * mouse_sensitivity;
+
+            transform.Rotate(body_rotate * Time.deltaTime);
+            head.transform.Rotate(head_rotate * Time.deltaTime);
+        }
+        
+        move_direction = transform.TransformDirection(move_direction);
         character_controller.Move(move_direction * Time.deltaTime);
 
     }
