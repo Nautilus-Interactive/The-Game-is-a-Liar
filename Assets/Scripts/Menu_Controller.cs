a8c4e334e115468ba8fcaa3a0f88203ec1692548
NautilusInteractive <admin@NautilusInteractive.com>
2019-08-24 18:15:16 -0300

    Added basic menu
    Added menu with Play, Quit, & Credits. Credits button shows a window displaying the creators of the game

---
+++ Assets/Scripts/Menu_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Controller : MonoBehaviour
+{
+    public GameObject credits;
+    public void StartGame() {
+        Debug.Log("Loading Game Scene...");
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ShowCredits() {
+        Debug.Log("Loading Credit Panel...");
+        credits.SetActive(true);
+    }
+
+    public void HideCredits() {
+        Debug.Log("Loading Credit Panel...");
+        credits.SetActive(false);
+    }
+
+    public void QuitGame() {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
