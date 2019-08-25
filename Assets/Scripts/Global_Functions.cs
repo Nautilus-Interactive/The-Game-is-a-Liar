f31be8d29669c1175f8077cdf1664b44bbb2e30f
NautilusInteractive <admin@NautilusInteractive.com>
2019-08-25 13:16:03 -0300

    Temp Checkin

--- Assets/Scripts/Global_Functions.cs
+++ Assets/Scripts/Global_Functions.cs
@@ -2,21 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class Game_Variables
+public static class Global_Functions
 {
-    public static bool paused = false;
-
     public static void start_game() {
         resume_game();
     }
 
     public static void pause_game() {
         Time.timeScale = 0;
-        paused = true;
     }
 
     public static void resume_game() {
         Time.timeScale = 1;
-        paused = false;
     }
 }
