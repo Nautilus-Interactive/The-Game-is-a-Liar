03b7c395200d34145571689f1aee53c1214083b7
NautilusInteractive <admin@NautilusInteractive.com>
2019-08-26 01:08:20 -0300

    Added UI to objects

--- Assets/Scripts/Evidence_Controller.cs
+++ Assets/Scripts/Evidence_Controller.cs
@@ -1,37 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Evidence_Controller : MonoBehaviour
 {
-    public float pickup_distance = 2.0f;
-
+    public float pickup_distance = 3.0f;
     public bool can_pickup = false;
-
     private GameObject player;
+    public string name;
+    public GameObject UI;
 
     private void Start() {
-        player = GameObject.Find("Player");    
+        player = GameObject.Find("Player");
+        UI.transform.Find("Name").GetComponent<TextMeshProUGUI>().text = name;
+        UI.transform.Find("Pickup").gameObject.SetActive(can_pickup);
     }
 
-    private void OnMouseEnter() {
-        Debug.Log("Display Info");
-
-        if (can_pickup) {
-
+    private void OnMouseOver() {
+        Vector3 distance = player.transform.position - this.transform.position;
+        if (distance.magnitude < pickup_distance) {
+            ShowInfo();
         }
-
-
+        else {
+            HideInfo();
+        }
     }
 
     private void OnMouseExit() {
-        
+        HideInfo();
     }
 
     private void OnMouseDown() {
-        Debug.Log(Vector3.Distance(player.transform.position, this.transform.position));
+        Vector3 distance = player.transform.position - this.transform.position;
+        if (distance.magnitude < pickup_distance) {
+            Pickup();
+        }
+    }
+
+    void ShowInfo() {
+        //Debug.Log("Display Info");
+        UI.SetActive(true);
+    }
+
+    void HideInfo() {
+        //Debug.Log("Display Info");
+        UI.SetActive(false);
+    }
+
+    void Pickup() {
         Debug.Log("PickUp");
-        Global_Functions.add_inventory(this.gameObject);        
+        Global_Functions.add_inventory(this.gameObject);
         Destroy(this.gameObject);
     }
 }
