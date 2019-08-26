18bf2f0398ef8d73a38cdec9e556c3be580a6eb0
NautilusInteractive <admin@NautilusInteractive.com>
2019-08-25 21:25:24 -0300

    Added objects and basic script

---
+++ Assets/Scripts/Evidence_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evidence_Controller : MonoBehaviour
+{
+    public float pickup_distance = 2.0f;
+
+    private GameObject player;
+
+    private void Start() {
+        player = GameObject.Find("Player");    
+    }
+
+    private void OnMouseEnter() {
+        Debug.Log("Display Info");
+    }
+
+    private void OnMouseDown() {
+        Debug.Log(Vector3.Distance(player.transform.position, this.transform.position));
+        if (Vector3.Distance(player.transform.position, this.transform.position) <= pickup_distance) {
+            Debug.Log("PickUp");
+            Destroy(this.gameObject);
+        }
+    }
+}
