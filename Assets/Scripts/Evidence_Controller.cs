9bc34c51404c30e5b6cbf779fd5c40213ecb4298
NautilusInteractive <admin@NautilusInteractive.com>
2019-08-26 01:16:24 -0300

    Started switching over to using rays

--- Assets/Scripts/Evidence_Controller.cs
+++ Assets/Scripts/Evidence_Controller.cs
@@ -5,52 +5,16 @@ using TMPro;
 
 public class Evidence_Controller : MonoBehaviour
 {
-    public float pickup_distance = 3.0f;
-    public bool can_pickup = false;
-    private GameObject player;
-    public string name;
-    public GameObject UI;
+    public float max_distance = 3.0f;
 
-    private void Start() {
-        player = GameObject.Find("Player");
-        UI.transform.Find("Name").GetComponent<TextMeshProUGUI>().text = name;
-        UI.transform.Find("Pickup").gameObject.SetActive(can_pickup);
-    }
-
-    private void OnMouseOver() {
-        Vector3 distance = player.transform.position - this.transform.position;
-        if (distance.magnitude < pickup_distance) {
-            ShowInfo();
+    private void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.transform.gameObject.tag == "Interactable") {
+                Debug.Log("hit");
+                Debug.Log(hit.transform.gameObject.name);
+            }
         }
-        else {
-            HideInfo();
-        }
-    }
-
-    private void OnMouseExit() {
-        HideInfo();
-    }
-
-    private void OnMouseDown() {
-        Vector3 distance = player.transform.position - this.transform.position;
-        if (distance.magnitude < pickup_distance) {
-            Pickup();
-        }
-    }
-
-    void ShowInfo() {
-        //Debug.Log("Display Info");
-        UI.SetActive(true);
-    }
-
-    void HideInfo() {
-        //Debug.Log("Display Info");
-        UI.SetActive(false);
-    }
-
-    void Pickup() {
-        Debug.Log("PickUp");
-        Global_Functions.add_inventory(this.gameObject);
-        Destroy(this.gameObject);
     }
 }
