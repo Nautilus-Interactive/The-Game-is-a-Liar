cd6d7cc3252ace8732cc283f4482dbce41907011
NautilusInteractive <admin@NautilusInteractive.com>
2019-08-28 21:20:16 -0300

    Renamed to correct spelling

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialog : MonoBehaviour {
+public class Dialogue : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
 
