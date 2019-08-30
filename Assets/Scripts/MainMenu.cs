06b0cc9b3dfac0eb2c910bdd6dbad9439875d4a0
NautilusInteractive <admin@NautilusInteractive.com>
2019-08-30 13:46:40 -0300

    Temp

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,14 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     public GameObject credits;
+    public GameObject options;
 
     public void StartGame() {
         Debug.Log("Loading Game Scene...");
         SceneManager.LoadScene("Game");
     }
 
+    // Options Screen Methods
+    public void ShowOptions() {
+        Debug.Log("Loading Options Panel...");
+        HideCredits();
+        options.SetActive(true);
+    }
+
+    public void HideOptions() {
+        Debug.Log("Loading Options Panel...");
+        options.SetActive(false);
+    }
+
+    // Credits Screen Methods
     public void ShowCredits() {
         Debug.Log("Loading Credit Panel...");
+        HideOptions();
         credits.SetActive(true);
     }
 
