414f3bff6512622e30cedef45457ba0ba65fccba
NautilusInteractive <admin@NautilusInteractive.com>
2019-08-30 14:28:44 -0300

    Move images and menu changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour {
     public GameObject credits;
     public GameObject options;
+    public GameObject controls;
 
     public void StartGame() {
         Debug.Log("Loading Game Scene...");
@@ -16,6 +17,7 @@ public class MainMenu : MonoBehaviour {
     public void ShowOptions() {
         Debug.Log("Loading Options Panel...");
         HideCredits();
+        HideControls();
         options.SetActive(true);
     }
 
@@ -24,10 +26,24 @@ public class MainMenu : MonoBehaviour {
         options.SetActive(false);
     }
 
+    // Controls Screen Methods
+    public void ShowControls() {
+        Debug.Log("Loading Controls Panel...");
+        HideCredits();
+        HideOptions();
+        controls.SetActive(true);
+    }
+
+    public void HideControls() {
+        Debug.Log("Loading Controls Panel...");
+        controls.SetActive(false);
+    }
+
     // Credits Screen Methods
     public void ShowCredits() {
         Debug.Log("Loading Credit Panel...");
         HideOptions();
+        HideControls();
         credits.SetActive(true);
     }
 
